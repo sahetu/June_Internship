51e0d845ae5f57539936240676c0813b765d6ce5
Sagar Khatri <khatrisagar2@gmail.com>
2023-06-22 12:00:56 +0530

    22nd June

--- app/src/main/java/june/internship/ForgotPasswordActivity.java
+++ app/src/main/java/june/internship/ForgotPasswordActivity.java
@@ -3,12 +3,74 @@ package june.internship;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+    EditText email,password,confirmPassword;
+    Button continueButton,submitButton;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        email = findViewById(R.id.forgot_password_email);
+        password = findViewById(R.id.forgot_password);
+        confirmPassword = findViewById(R.id.forgot_confirm_password);
+
+        continueButton = findViewById(R.id.forgot_password_continue);
+        submitButton = findViewById(R.id.forgot_password_submit);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.getText().toString().trim().equals("")){
+                    email.setError("Mail Id Required");
+                }
+                else if(!email.getText().toString().matches(emailPattern)){
+                    email.setError("Valid Mail Id Required");
+                }
+                else{
+                    email.setEnabled(false);
+                    password.setVisibility(View.VISIBLE);
+                    confirmPassword.setVisibility(View.VISIBLE);
+
+                    continueButton.setVisibility(View.GONE);
+                    submitButton.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Min. 6 Char Password Required");
+                }
+                else if(confirmPassword.getText().toString().trim().equals("")){
+                    confirmPassword.setError("Confirm Password Required");
+                }
+                else if(confirmPassword.getText().toString().trim().length()<6){
+                    confirmPassword.setError("Min. 6 Char Confirm Password Required");
+                }
+                else if(!confirmPassword.getText().toString().matches(password.getText().toString())){
+                    confirmPassword.setError("Confirm Password Does Not Match");
+                }
+                else{
+                    Toast.makeText(ForgotPasswordActivity.this, "Password Changed Successfully", Toast.LENGTH_SHORT).show();
+                    onBackPressed();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/june/internship/SignupActivity.java
+++ app/src/main/java/june/internship/SignupActivity.java
@@ -2,13 +2,113 @@ package june.internship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class SignupActivity extends AppCompatActivity {
+
+    EditText name,email,contact,password,confirmPassword,dob;
+    Button signup;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        name = findViewById(R.id.signup_name);
+        email = findViewById(R.id.signup_email);
+        contact = findViewById(R.id.signup_contact);
+        password = findViewById(R.id.signup_password);
+        confirmPassword = findViewById(R.id.signup_confirm_password);
+
+        dob = findViewById(R.id.signup_dob);
+
+        calendar = Calendar.getInstance();
+
+        DatePickerDialog.OnDateSetListener dateClick = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(Calendar.YEAR,i);
+                calendar.set(Calendar.MONTH,i1);
+                calendar.set(Calendar.DAY_OF_MONTH,i2);
+
+                SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                dob.setText(sdf.format(calendar.getTime()));
+
+            }
+        };
+
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog dialog = new DatePickerDialog(SignupActivity.this,dateClick,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+
+                //Select Past Date
+                //dialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+                //Select Future Date
+                dialog.getDatePicker().setMinDate(System.currentTimeMillis());
+
+                dialog.show();
+            }
+        });
+
+        signup = findViewById(R.id.signup_button);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(name.getText().toString().trim().equals("")){
+                    name.setError("Name Required");
+                }
+                else if(email.getText().toString().trim().equals("")){
+                    email.setError("Email Id Required");
+                }
+                else if(!email.getText().toString().trim().matches(emailPattern)){
+                    email.setError("Valid Email Id Required");
+                }
+                else if(contact.getText().toString().trim().equals("")){
+                    contact.setError("Contact No. Required");
+                }
+                else if(contact.getText().toString().trim().length()<10){
+                    contact.setError("Valid Contact No. Required");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Min. 6 Char Password Required");
+                }
+                else if(confirmPassword.getText().toString().trim().equals("")){
+                    confirmPassword.setError("Confirm Password Required");
+                }
+                else if(confirmPassword.getText().toString().trim().length()<6){
+                    confirmPassword.setError("Min. 6 Char Confirm Password Required");
+                }
+                else if(!confirmPassword.getText().toString().trim().matches(password.getText().toString())){
+                    confirmPassword.setError("Confirm Password Does Not Match");
+                }
+                else if(dob.getText().toString().trim().equals("")){
+                    Toast.makeText(SignupActivity.this, "Please Select Date Of Birth", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(SignupActivity.this, "Signup Successfully", Toast.LENGTH_SHORT).show();
+                    onBackPressed();
+                }
+            }
+        });
+
     }
 }
